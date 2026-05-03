23a1c90e11e482fb685465c6d40c58e3e8d3f422
rpan2022 <98272413+rpan2022@users.noreply.github.com>
2026-05-03 20:51:55 +0200

    Update clang tidy and commit hooks

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 #include <vector>
 
 auto main() -> int {
-  std::cout << "Hello C++23 with Clang-21 + CMake + Ninja!" << std::endl;
+  std::cout << "Hello C++23 with Clang-21 + CMake + Ninja!\n";
+
+  [[maybe_unused]] const int result = 1;
 
   std::print("current cpp version: {}\n", __cplusplus);
 
   std::vector<int> val{1, 2, 3, 4, 5};
 
-  for (auto& v : val) {
-    std::print("value = {}\n", v);
+  for (auto& tmp : val) {
+    std::print("value = {}\n", tmp);
   }
 
   return 0;
